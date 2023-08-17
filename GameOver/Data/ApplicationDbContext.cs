2dd442b067ebe3a04c024896f02d9705f34fe3ba
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-08-17 17:22:56 +0300

    Update Env ORM

---
+++ GameOver/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameOver.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
--- GameOver/Program.cs
+++ GameOver/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
