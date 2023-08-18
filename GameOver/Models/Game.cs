5dc4a747680e986c16d8c40fe2fe5addf7dffa0d
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-08-18 15:27:15 +0300

    Domain Models + Games

---
+++ GameOver/Models/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameOver.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string Description { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string Cover { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = default!;
+    }
+}
