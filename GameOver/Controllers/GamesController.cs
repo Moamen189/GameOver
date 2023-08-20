b80d51cac7531ae7a700300645c3b613be2d0166
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-08-20 10:25:01 +0300

    FController

---
+++ GameOver/Controllers/GamesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameOver.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
