12d948e752f1fdeee2912017be18480d6f32826b
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-08-21 09:17:14 +0300

    ViewModel

---
+++ GameOver/ViewModels/CreateGameViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace GameOver.ViewModels
+{
+	public class CreateGameViewModel
+	{
+
+		[MaxLength(250)]
+		public string Name { get; set; } = string.Empty;
+
+
+		[MaxLength(1000)]
+		public string Description { get; set; } = string.Empty;
+
+		public int CategoryId { get; set; }
+
+		public List<int> SelectedDevices { get; set; } = new List<int>();
+		public IFormFile Cover { get; set; } = default!;
+
+	}
+}
