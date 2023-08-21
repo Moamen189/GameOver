96b71b088228fb35938db3219b5f05f8673c582b
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-08-21 09:34:02 +0300

    Update ViewModel For Games

--- GameOver/ViewModels/CreateGameViewModel.cs
+++ GameOver/ViewModels/CreateGameViewModel.cs
@@ -1,19 +1,24 @@
-﻿namespace GameOver.ViewModels
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GameOver.ViewModels
 {
 	public class CreateGameViewModel
 	{
 
 		[MaxLength(250)]
 		public string Name { get; set; } = string.Empty;
-
-
-		[MaxLength(1000)]
-		public string Description { get; set; } = string.Empty;
-
 		public int CategoryId { get; set; }
 
 		public List<int> SelectedDevices { get; set; } = new List<int>();
 
+        public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+
+		public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
+
+		[MaxLength(1000)]
+		public string Description { get; set; } = string.Empty;
+
+		//form validation
 		public IFormFile Cover { get; set; } = default!;
 
 	}
