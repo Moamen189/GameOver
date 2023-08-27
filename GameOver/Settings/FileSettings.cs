5989687af55f12d0c7f4893a1533d3d9e5110c71
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-08-27 10:23:57 +0300

    FileSettings

---
+++ GameOver/Settings/FileSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace GameOver.Settings
+{
+	public static class FileSettings
+	{
+		public const string ImagePath = "/assets/images/games";
+
+		public const string AllowsExtenstions = ".jpg,.jpeg,.png";
+
+		public const int MaxFileSizeInMB = 1;
+		public const int MaxFileSizeInBytes = MaxFileSizeInMB * 1024 * 1024;
+
+	}
+}
