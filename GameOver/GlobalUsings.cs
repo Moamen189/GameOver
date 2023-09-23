8709ba2ca83b7ba744e0a92a3960b0089731c21b
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-09-23 13:39:19 +0300

    View Details

--- GameOver/GlobalUsings.cs
+++ GameOver/GlobalUsings.cs
@@ -3,4 +3,4 @@ global using Microsoft.EntityFrameworkCore;
 global using GameOver.ViewModels;
 global using Microsoft.AspNetCore.Mvc;
 global using System.ComponentModel.DataAnnotations;
- 
+global using GameOver.Settings;
