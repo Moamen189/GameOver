4a0204c4e500ef409eb7f808bb650e9b1e3f3316
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-09-23 14:38:19 +0300

    UpdateCreateViewModel

--- GameOver/ViewModels/CreateGameViewModel.cs
+++ GameOver/ViewModels/CreateGameViewModel.cs
@@ -4,30 +4,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GameOver.ViewModels
 {
-	public class CreateGameViewModel
+	public class CreateGameViewModel:GameFormViewModel
 	{
 
-		[MaxLength(250)]
-		public string Name { get; set; } = string.Empty;
+        [AllowedExtensions(FileSettings.AllowsExtenstions),
+            MaxFileSize(FileSettings.MaxFileSizeInBytes)]
+        public IFormFile? Cover { get; set; } = default!;
 
-		[Display(Name = "Category")]
-		public int CategoryId { get; set; }
-		[Display(Name = " Supported Devices")]
-
-		public List<int> SelectedDevices { get; set; } = default!;
-
-		//for Selection by IEnumrable
-        public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
-
-		public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
-
-		[MaxLength(1000)]
-		public string Description { get; set; } = string.Empty;
-
-		//form validation
-		[AllowedExtensions(FileSettings.AllowsExtenstions) , MaxFileSize(FileSettings.MaxFileSizeInBytes)]
-		
-		public IFormFile Cover { get; set; } = default!;
-
-	}
+    }
 }
